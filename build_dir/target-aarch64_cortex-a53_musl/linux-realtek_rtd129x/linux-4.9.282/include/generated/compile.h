83e33c0221a7b83d80e5c0c26176acebecb4f447
leenchan <leenchan@gmail.com>
2022-01-14 14:20:35 +0000

    2022-01-14 18:44

--- build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/generated/compile.h
+++ build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 0 */
 /* SMP */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#0 SMP Fri Dec 31 08:38:34 2021"
+#define UTS_VERSION "#0 SMP Fri Jan 14 07:03:16 2022"
 #define LINUX_COMPILE_BY "build"
 #define LINUX_COMPILE_HOST "buildserver"
 #define LINUX_COMPILER "gcc version 7.5.0 (OpenWrt GCC 7.5.0 r0-42f5b70) "
--- build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/linux/skbuff.h
+++ build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/linux/skbuff.h
@@ -2034,21 +2034,6 @@ static inline unsigned char *__skb_put(struct sk_buff *skb, unsigned int len)
 	return tmp;
 }
 
-static inline void *skb_put_data(struct sk_buff *skb, const void *data,
-				 unsigned int len)
-{
-	void *tmp = (void *)skb_put(skb, len);
-
-	memcpy(tmp, data, len);
-
-	return tmp;
-}
-
-static inline void skb_put_u8(struct sk_buff *skb, u8 val)
-{
-	*(u8 *)skb_put(skb, 1) = val;
-}
-
 unsigned char *skb_push(struct sk_buff *skb, unsigned int len);
 static inline unsigned char *__skb_push(struct sk_buff *skb, unsigned int len)
 {
