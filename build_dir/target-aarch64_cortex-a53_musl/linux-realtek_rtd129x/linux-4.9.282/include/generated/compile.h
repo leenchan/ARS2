dafeedc76cd34568da05d799c9e8319dfc21504b
leenchan <leenchan@gmail.com>
2023-03-11 17:30:40 +0000

    2023-03-10 11:40

--- build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/generated/compile.h
+++ build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 0 */
 /* SMP */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#0 SMP Wed Feb 22 10:40:11 2023"
+#define UTS_VERSION "#0 SMP Thu Mar 9 09:06:39 2023"
 #define LINUX_COMPILE_BY "build"
 #define LINUX_COMPILE_HOST "buildserver"
 #define LINUX_COMPILER "gcc version 7.5.0 (OpenWrt GCC 7.5.0 r16747-8cfec6b49c) "
